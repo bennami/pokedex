b59bf320ea71634dbfeceb0889df95481bd53b38
imane <benbenimane@hotmail.com>
2020-01-21 18:25:14 +0100

    getting data from api, icon pokemon,...

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,24 +1,22 @@
 //things we need from html
-const pokeIcon = document.querySelector('pokIcon');
-const pokeName = document.querySelector('pokName');
-const pokeDescription = document.querySelector('description');
-const poeMoves = document.querySelector('movesList');
-const evolutionIcon = document.querySelector('evolutionIcon');
-const evolutionName = document.querySelector('evolutionName');
+let pokeIcon = document.querySelector('.pokeIcon');
+let pokeName = document.querySelector('.pokeName');
+const pokeDescription = document.querySelector('.description');
+const poeMoves = document.querySelector('.movesList');
+const evolutionIcon = document.querySelector('.evolutionIcon');
+const evolutionName = document.querySelector('.evolutionName');
 
 //click event
 document.getElementById('inputBtn').addEventListener('click', function getName(){
 
- //get input name
  let inputName = document.getElementById('input').value;
-
 //invoke function to fetch JSON with input name as parameter
- getData(inputName);
-
+ getPokemon(inputName);
+//put name back on html
+pokeName.innerHTML = inputName;
 });
-
 //fetch JSON
-async function getData(name) {
+async function getPokemon(name) {
 
 //fetch stream of data
 const response1= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
@@ -28,6 +26,16 @@ const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}
 const pObject =  await response1.json();
 const pSpecies =   await response2.json();
 
+//get Poke icon
+let URL = pObject.sprites.front_default;
+let id = pObject.id;
+let types =pObject.types.map((type) => type.type.name).join(', ');
+pokeIcon.src = URL;
 console.log(pObject);
 console.log(pSpecies);
+console.log(types);
+
 }
+
+
+
