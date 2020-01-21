dd353ed248fe892c7f950dfaf0bf3709b883ed6c
imane <benbenimane@hotmail.com>
2020-01-21 19:46:09 +0100

    get description, icon, evicon ...

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,7 +4,7 @@ let pokeName = document.querySelector('.pokeName');
 const pokeDescription = document.querySelector('.description');
 const poeMoves = document.querySelector('.movesList');
 const evolutionIcon = document.querySelector('.evolutionIcon');
-const evolutionName = document.querySelector('.evolutionName');
+let evolutionName = document.querySelector('.evolutionName');
 
 //click event
 document.getElementById('inputBtn').addEventListener('click', function getName(){
@@ -19,23 +19,46 @@ pokeName.innerHTML = inputName;
 async function getPokemon(name) {
 
 //fetch stream of data
-const response1= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+const response1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
 const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
+
 
 //convert to json
 const pObject =  await response1.json();
 const pSpecies =   await response2.json();
 
-//get Poke icon
-let URL = pObject.sprites.front_default;
+//get pre evolutioon name
+let preEvolutionP = pSpecies.evolves_from_species.name;
+let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${preEvolutionP}`);
+let pEvolution = await res.json();
+
+evolutionIcon.src = pEvolution.sprites.front_default;
+evolutionName.innerHTML = preEvolutionP;
+
+//get Poke data
 let id = pObject.id;
-let types =pObject.types.map((type) => type.type.name).join(', ');
-pokeIcon.src = URL;
-console.log(pObject);
-console.log(pSpecies);
-console.log(types);
+let types = pObject.types.map((type) => type.type.name).join(', ');
+pokeIcon.src = pObject.sprites.front_default;
+pokeDescription.innerHTML = pSpecies.flavor_text_entries[2].flavor_text;
+
+//get all moves in array
+let moves=[];
+  for(i=0; i< pObject.moves.length;i++){
+
+    let allMoves = pObject.moves[i].move.name;
+    moves.push(allMoves);
+
+  }
+//filter out 4 random moves
+ for(i=0;i<4;i++){
+ var randomItem = moves[Math.floor(Math.random()*moves.length)];
+ }
+ console.log(randomItem);
+
+
 
 }
+
 
 
 
